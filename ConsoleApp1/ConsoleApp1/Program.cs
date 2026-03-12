7d57f7cdbb20e7e1242a5f7fd5bab53c247fbdfe
Szymon Szot <s33692@pjwstk.edu.pl>
2026-03-12 09:24:14 +0100

    null vector error

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,3 +4,8 @@ Console.WriteLine("Input vector data (a, b, c)");
 string a = Console.In.ReadLine();
 Console.WriteLine("Input second vector data (a, b, c)");
 string b = Console.In.ReadLine();
+
+if (a == null || b == null)
+{
+    Console.WriteLine("input error");
+}
