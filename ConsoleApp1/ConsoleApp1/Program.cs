abfa632f3c59b404d5d5cd0806c160d75120d04c
Szymon Szot <s33692@pjwstk.edu.pl>
2026-03-12 09:22:23 +0100

    User input

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Input vector data (a, b, c)");
+string a = Console.In.ReadLine();
+Console.WriteLine("Input second vector data (a, b, c)");
+string b = Console.In.ReadLine();
